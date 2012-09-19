8ed98953b20a15e517b67fc08178c912fdd0f662
Greg Turner <greg@interaction.net.au>
2012-09-19 21:18:08 +1000

    deprecating in favour of ixc

--- stopspam/forms/__init__.py
+++
@@ -1,5 +0,0 @@
-#Don't use import * - causes all sorts of namespace clashes
-
-from widgets import RecaptchaResponse, RecaptchaChallenge, HoneypotWidget
-from fields import HoneypotField
-from forms import BaseForm, AkismetForm, RecaptchaForm, HoneyPotForm, SuperSpamKillerForm
